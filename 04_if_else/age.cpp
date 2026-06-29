d07c1a16257a5df6ac0754470e99bb1d8f0338e9
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-06-29 23:13:51 +0530

    if else statement

---
+++ 04_if_else/age.cpp
@@ -0,0 +1,17 @@
+// age if else statement
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cout << " Enter your age";
+    cin >> age;
+    if(age > 18){
+        cout << "you are eligible";
+    }
+    else{
+        cout << "you are not eligible";
+    }
+return 0;
+}
