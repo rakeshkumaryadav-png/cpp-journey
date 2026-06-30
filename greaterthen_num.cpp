401f132965b599443a2f1e0f49e29f677b3dbe0f
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-06-30 21:25:24 +0530

    number

---
+++ greaterthen_num.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout << "Enter first number" << endl;
+    cin >> a ;
+    cout << "Enter second number" << endl;
+    cin >> b;
+    if(a > b){
+        cout << " A is greterthen" << endl;
+    }
+    else{
+        cout << " B is greterthen" << endl;
+    }
+return 0;
+}
