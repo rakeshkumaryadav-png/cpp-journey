94c83da6c5e7dce9ae1d57d293d144cebe3dc0cb
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-07-09 16:53:50 +0530

    pattern

--- 09_pattern/05_pattern.cpp
+++ 09_pattern/05_pattern.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main (){
-for(int i = 1; i<= 4; i++ ){
+for(int i = 4; i>= 1; i-- ){
     for(int j= 1; j <= i; j++){
-        cout << i;
+        cout << "*";
     }
     cout << endl;
 }
