6d3ddb827218f6cee069cd5ee38c3b104cfe5fe1
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-07-14 09:58:06 +0530

    pattern

--- 09_pattern/16_pattern.cpp
+++ 09_pattern/16_pattern.cpp
@@ -8,3 +8,9 @@ int main(){
         cout << endl;
     }
 }
+
+
+// A
+// BB
+// CCC
+// DDDD
---
+++ 09_pattern/17_pattern.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "enter number";
+    cin >> n;
+    for(int i = 1; i<= n; i++){
+        for(int j = n - i +1; j<= n; j++){
+            cout << char('A' + j - 1);
+        }
+        cout << endl;
+    }
+    return 0;
+}
