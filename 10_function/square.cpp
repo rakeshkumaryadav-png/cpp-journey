694c0a815a02ddc0a713b0229140c3c22006b4d3
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-07-18 07:59:36 +0530

    function

---
+++ 10_function/square.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int square(int n){
+    return n * n;
+}
+int main(){
+    int num;
+cout << "Enter a nummber";
+cin >> num;
+
+cout << "Square =" << square(num);
+return 0;
+}
+
+
+// Enter a nummber 6
+// Square =36
