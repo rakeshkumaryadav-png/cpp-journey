ca4625784dc2e4208c31e162a5f559a0f7b75118
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-07-19 20:10:12 +0530

    array

---
+++ 11_basic_array/minimum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int arr[5];
+    cout << "Enter 5 element";
+for (int i = 0; i < 5; i++){
+    cin >>  arr[i];
+}
+    int max = arr[0];
+for(int i = 4; i > 0; i--){
+    if(arr[i] < max){
+        max = arr[i];
+    }
+}
+cout << "Minimum = " << max;
+    return 0;
+}
