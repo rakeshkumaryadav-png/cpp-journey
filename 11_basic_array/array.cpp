994ce03728da21df64ad47125b2649cbf2fa04af
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-07-19 12:26:56 +0530

    array

---
+++ 11_basic_array/array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int arr[5];
+    cout << "Enter 5 element";
+for (int i = 0; i < 5; i++){
+    cin >>  arr[i];
+}
+cout << "Array element";
+for(int i = 0; i < 5; i++){
+    cout << arr[i] << " ";
+}
+    return 0;
+}
+
+
+// Enter 5 element 1 2 3 4 5 5
+// Array element1 2 3 4 5 
