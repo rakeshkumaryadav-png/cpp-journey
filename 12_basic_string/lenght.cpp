cdfc7080a7250e00b8e6777a98313f521109d97d
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-07-23 20:41:00 +0530

    string

---
+++ 12_basic_string/lenght.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+    cout << "Enter name";
+    getline(cin, name);
+    cout << "lenght = " << name.length();
+    return 0;
+}
+
+// Enter namerakesh
+// lenght = 6
