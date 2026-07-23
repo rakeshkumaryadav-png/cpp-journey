ccd483411b0d1363bf66bf94468cfc825ebcbbd8
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-07-23 20:35:43 +0530

    string

---
+++ 12_basic_string/string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+    cout << "Enter your name";
+    getline(cin, name);
+
+    cout << "Hello" << name;
+    return 0;
+}
+
+
+// Enter your name rakesh yadav
+// Hello  rakesh yadav
