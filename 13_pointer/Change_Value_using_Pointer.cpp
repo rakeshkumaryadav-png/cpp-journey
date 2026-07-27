bef1b16c0f83ee96dd6bbd48b02ffc625ceeccc6
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-07-27 20:24:38 +0530

    pointer

---
+++ 13_pointer/Change_Value_using_Pointer.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int *p = &num;
+
+    *p = 20;
+
+    cout << "Updated Number = " << num << endl;
+
+    return 0;
+}
+
+// Enter a number: 50
+// Updated Number = 20
