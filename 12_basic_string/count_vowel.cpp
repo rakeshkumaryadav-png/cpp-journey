ed702d0b2d6d219e4af313390ad0842111274f05
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-08-13 21:11:44 +0530

    basic math

---
+++ 12_basic_string/count_vowel.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout << "enter number";
+    cin >>  num;
+    if(num > 0){
+        cout << "positive";
+    }
+    else if(num < 0){
+        cout << "hdhfhdg";
+    }
+    else{
+        cout << "zero";
+    }
+    return 0;
+}
