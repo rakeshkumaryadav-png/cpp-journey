fd2420b4ff186135cea55ba743133d69e1ae3faa
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-08-16 21:44:54 +0530

    STL

---
+++ 16_STL_vector/empty.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> v;
+    if(v.empty()){
+        cout  <<"vector is empty";
+    }
+    else{
+        cout <<"vector is not empty";
+    }
+    return 0;
+}
+
+
+// vector is empty
