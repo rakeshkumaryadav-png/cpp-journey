bb770551e57eb3905d467444efa7b7b9a8d19695
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-08-17 16:01:07 +0530

    STL

---
+++ 17_STL_pair/pair.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    pair <int,string> p;
+
+    p.first = 101;
+    p.second ="Rakesh";
+
+    cout << p.first << endl;
+    cout << p.second << endl;
+    return 0;
+}
+
+
+// 101
+// Rakesh
