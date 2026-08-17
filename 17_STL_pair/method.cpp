fcb5c7926acb8d2590ad286de6e4eaf88917dac4
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-08-17 16:05:51 +0530

    STL

---
+++ 17_STL_pair/method.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<utility>
+using namespace std;
+int main(){
+    pair<int,char> p = {50, 'A'};
+
+    cout <<p.first<< endl;
+    cout <<p.second << endl;
+    return 0;
+}
+
+
+// 50
+// A
