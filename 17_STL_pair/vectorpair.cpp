862b11081a810882fd5926a459f9736794c7ad27
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-08-18 21:47:33 +0530

    STL

--- 17_STL_pair/vectorpair.cpp
+++ 17_STL_pair/vectorpair.cpp
@@ -11,12 +11,15 @@ int main() {
     v.push_back({102, 90});
 
     for(int i =0; i <v.size(); i++){
-       cout << v[i].first << " " << v[i].second << endl;
+        cout << "Roll no = " <<  v[i].first;
+       cout << "marks = " <<  v[i].second << endl;
     
     }
     
     return 0;
 }
 
-// 101 85
-// 102 90
+
+// Roll no = 101marks = 85
+// Roll no = 102marks = 90
+    
