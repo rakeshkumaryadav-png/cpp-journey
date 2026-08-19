28dab1b0616250769df75efca51925fbb9c6bad8
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-08-19 21:26:39 +0530

    STL

---
+++ 18_stack/empty.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main(){
+    stack<int> s;
+    if(s.empty()){
+        cout << "stack is empty";
+    }
+    else{
+        cout << "stack is empty";
+    }
+
+    return 0;
+}
+
+// stack is empty
