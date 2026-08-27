f93fe0f6aa2f2dbf04428450e805c5fcc85fadfb
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-08-27 22:17:52 +0530

    STL

---
+++ 23_Multiset/clear.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <set>
+using namespace std;
+int main(){
+    multiset<int > ms;
+    ms.insert (10);
+    ms.insert(20);
+    ms.insert(30);
+
+  ms.clear();
+
+  cout<< ms.size();
+    return 0;
+}
+
+
+// 0
