4b7455f0eba3e19e2e7ef5ab7f0db3f4e06c62a3
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-09-02 21:57:53 +0530

    STL

---
+++ 27_unordered_map/begin.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <unordered_map>
+using namespace std;
+int main(){
+    unordered_map <int, string> mp;
+    mp[101] = "Rakesh";
+    mp[102] = "Aman";
+    mp[103] = "Vikash";
+
+    for(auto it = mp.begin(); it != mp.end(); it++){
+        cout << it -> first << " -> " << it-> second << endl;
+    }
+}
+
+// 103 -> Vikash
+// 102 -> Aman
+// 101 -> Rakesh
