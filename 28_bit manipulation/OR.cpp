07f5bbb06742f6a69add564b5645ed0c79f6d434
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-09-03 22:00:52 +0530

    STl

---
+++ 28_bit manipulation/OR.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a , b;
+    cout << "enter two number";
+    cin >> a >> b;
+   
+
+    cout << (a | b);
+    return 0;
+}
+
+// enter two number 5 3
+// 7
