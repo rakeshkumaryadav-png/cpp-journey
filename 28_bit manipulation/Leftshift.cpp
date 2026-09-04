78d61b583b95a9198d019ce63932e866174004f6
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-09-04 14:43:54 +0530

    STL

---
+++ 28_bit manipulation/Leftshift.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout <<"enter a number";
+    cin>> n;
+
+    cout <<(n<< 1);
+    return 0;
+}
+
+
+// enter a number 5
+// 10
