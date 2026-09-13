6a6adb096dceebf148686d7e9455aeb579379412
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-09-13 22:43:36 +0530

    if else

---
+++ 04_if_else/vowel_consonant.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout << "enter alphabet";
+    cin >> ch;
+
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch== 'u'){
+        cout << "vowel";
+    }
+    else{
+        cout << "consonnant";
+    }
+}
+
+
+// enter alphabet t
+// consonnant
+
+
+// enter alphabet e
+// vowel
