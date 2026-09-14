063d0b61c5e4c3a11849c317c9c87f3d12ab9d9e
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-09-14 20:53:54 +0530

    basic math

---
+++ 15_basic_math/largest_num.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "enter number";
+    cin >> n;
+
+    int largest = 0;
+
+    while(n > 0){
+        int digit = n % 10;
+        if(digit > largest){
+            largest  = digit;
+        }
+        n = n / 10;
+    }
+    cout << largest;
+}
+
+
+// enter number 5323
+// 5
