216bbf648b2848e72f5fd7dd600a06989ea67f06
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-09-15 12:40:22 +0530

    basic maths

---
+++ 15_basic_math/divisor_of_num.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "enter number";
+    cin >> n;
+
+    cout <<"divisor";
+    for(int i = 1; i<= n; i++){
+        if(n % i == 0){
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
+
+
+
+// enter number 12
+// divisor1 2 3 4 6 12
