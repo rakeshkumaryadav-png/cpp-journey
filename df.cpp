cdce25ede30611821a081866b8796f24ce092793
rakesh kumar yadav <rakeshkumaryadav76442@gmail.com>
2026-09-20 22:06:29 +0530

    kferg

---
+++ df.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "enter a number";
+    cin >> n;
+    for(int i = 1;i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }k43tnjkem  erjkthutnet
+    cout << endl;
+    }
+
+
+
+    for(int i = 4; i>= 1; i-- ){
+    for(int j= 1; j <= i; j++){
+        cout << "*";
+    }
+    cout << endl;
+}
+return 0;
+} 
